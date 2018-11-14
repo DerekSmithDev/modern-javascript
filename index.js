fecb78415af5a2b9492f444ac4510e052331069a
dereksmith83 <mdsmith1983@gmail.com>
2018-11-13 21:49:31 -0600

    install moment package using npm

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hey");
+/* global moment */
+
+console.log("Hello from JavaScript!");
+//console.log("moment");
+var datetime = moment()
+  .startOf("day")
+  .fromNow();
+console.log("moment.js package: " + datetime);
